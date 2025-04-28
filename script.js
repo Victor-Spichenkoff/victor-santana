5cb5c366d768fa999f9ea34923b73ffef3d93d68
Victor-Spichenkoff <victorspich13@gmail.com>
2025-04-28 13:19:41 -0300

    Create contact form

--- script.js
+++ script.js
@@ -3,11 +3,28 @@ const inputs = document.querySelectorAll("nav a")
 
 const getInput = (documentName) => {
     inputs.forEach(input => {
-        
+
         console.log(input.innerText.toLowerCase() == documentName)
         if (input.innerText.toLowerCase() == documentName) {
-            input.classList.add("active")         
+            input.classList.add("active")
             input.addEventListener("click", (e) => e.preventDefault())
         }
     })
 }
+
+
+// EMAIL SEND
+const form = document.getElementById('contact-form')
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    const name = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    const message = document.getElementById('message').value
+
+    if(!name || !email || !message)
+        return window.alert("Preencha todos os campos")
+
+    window.alert("Enviado")
+})
